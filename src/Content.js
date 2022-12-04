74d962c8ef88852f64909575f6f1d66d17585cba
priteshparmar9 <74969871+priteshparmar9@users.noreply.github.com>
2022-12-04 16:54:14 +0530

    commit

--- src/Content.js
+++ src/Content.js
@@ -5,21 +5,24 @@ import Slider from "./Slider";
 import Card from "./Card";
 import Cards from "./Card";
 import MovieList from "./MovieList";
+import Loading from "./Loading";
 
 function Content() {
+  const [loading, setLoad] = useState(false);
   const [movie, setMovie] = useState([]);
 
   let url = 'https://moviebackend.onrender.com/movie/';
   useEffect(
     () => {
       function fetchData() {
+        setLoad(true);
         axios.get(url).then(
           (response) => {
 
             setMovie(response.data);
 
             console.log(movie);
-
+            setLoad(false);
           }
         ).catch(
           console.log('Error')
@@ -31,11 +34,21 @@ function Content() {
   
 
   return (
-    <div>
-    
-      <MovieList />
-      {/* {DisplayMovies()} */}
-    </div>
+    <>
+      {
+        loading?
+        <>
+          <Loading />
+        </>
+        :
+        <div>
+          <MovieList />
+        </div>
+        
+        
+
+      }
+    </>
 
   )
 }
---
+++ src/Loading.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './css/loading.css';
+
+function Loading() {
+    return (
+        <>
+        <div class="ring">Loading
+            <span class="span1"></span>
+        </div>
+        <div style={{
+            height:"1000px"
+        }}>
+
+        </div>
+        </>
+        
+    )
+}
+
+export default Loading;
